c40e5cf576f317c1cd98d5d06770ce137d8267b1
Dipak Chavan <chavandipak205@gmail.com>
2025-08-30 14:50:10 +0530

    style: update todo and app components with modern UI and fix Angular bindings

---
+++ src/app/todo/todo.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-todo',
+  standalone: true,
+  imports: [CommonModule, FormsModule],
+  templateUrl: './todo.html',
+  styleUrls: ['./todo.css']
+})
+export class Todo {
+  newTask: string = '';
+  tasks: string[] = [];
+
+  addTask() {
+    if (this.newTask.trim() !== '') {
+      this.tasks.push(this.newTask);
+      this.newTask = '';
+    }
+  }
+
+  removeTask(index: number) {
+    this.tasks.splice(index, 1);
+  }
+}
+
